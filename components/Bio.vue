18c7789afa4661d773f346e8bdf973568f96db34
mariajcb <bogomazm@gmail.com>
2020-05-17 17:07:35 -0400

    converts bio to markdown

--- components/Bio.vue
+++ components/Bio.vue
@@ -13,33 +13,7 @@
           </figure>
         </div>
         <div class="media-content">
-          <div class="content">
-            <h3 class="title is-marginless">Education</h3>
-            <p class="has-text-weight-semibold">Expert in group psychology, focusing on the quality of relationships and its impact on mood and happiness</p>
-              <ul>
-                <li>Graduated from the Illinois School of Professional Psychology in 2012</li>
-                <li>Training Director for the University of North Florida Counseling Center</li>
-                <li>One of the 5% of licensed psychologists who are board certified</li>
-                <li>Internationally Certified Group Psychotherapist</li>
-              </ul>
-            <h3 class="title is-marginless">Organizational Involvement</h3>
-            <p class="has-text-weight-semibold">Every professional should strive to be directly involved in shaping the future of their chosen field</p>
-              <ul>
-                <li>Board member-at-large at the American Psychological Association’s (APA) Division 49 (Group Psychology)</li>
-                <li>Co-treasurer for the American Board of Group Psychology</li>
-                <li>Examiner for psychologists seeking board certification in group psychology</li>
-                <li>Member of the board of the International Board Certification of Group Psychotherapists</li>
-                <li>Member of Committee of APA and Division Relations (CODAPAR), tasked with training the president-elects of new divisions, as well as reviewing grant submissions</li>
-              </ul>
-            <h3 class="title is-marginless">Professional Presentations</h3>
-              <p class="has-text-weight-semibold">The best way to learn information is to teach it</p>
-              <ul>
-                <li>China Institute of Psychology</li>
-                <li>American Psychological Association</li>
-                <li>American Group Psychotherapy Association</li>
-                <li>Jacksonville Naval Base</li>
-              </ul>
-          </div>
+          <div class="content" v-html="$options.filters.markdown(blok.text)"></div>
           <nuxt-link class="button is-medium" to="/contact">{{ blok.call_to_action_btn }}</nuxt-link>
         </div>
       </div>
@@ -54,10 +28,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-p {
-  margin-top: .2rem;
-}
-
 .button {
   background-color: #718FCB;
   margin-top: auto;
