fe627a55e3a933ca2f47a271f20a6e22c9b6c24d
mariajcb <bogomazm@gmail.com>
2020-05-22 18:16:27 -0400

    working on about me

---
+++ components/About.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div v-editable="blok" class="card about">
+    <h2>{{ blok.body }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['blok']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
